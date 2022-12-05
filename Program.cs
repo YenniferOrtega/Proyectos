4652ba8b7c822f66b1ef5c61208bb1a595287070
Yennifer Ortega Ocampo <Yenniferso.ortegaoc@ecci.Edu.co>
2022-12-04 19:58:09 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PROYECTO1NETCORE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 10;
+            int twoyear = Convert.ToInt16("convertir datos de tipo String a int");
+            double decimales = 2.5;
+            string nombre = "joe";
+
+            Console.WriteLine("Holaaaaaa tengo {0} "+ year + " años "+ decimales + nombre);
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
