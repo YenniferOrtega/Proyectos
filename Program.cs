db552d376bb70d35d5789c9f9c05696489450e86
Yennifer Ortega Ocampo <Yenniferso.ortegaoc@ecci.Edu.co>
2022-12-04 22:02:29 -0500

    if

--- Program.cs
+++ Program.cs
@@ -29,18 +29,83 @@ namespace PROYECTO1NETCORE
 
             //OPERADORES ARITMETICOS
 
-            int v = 10;
-            int v1 = 34;
-            var su = v+v1;
-            var re = v - v1;
-            var mul = v * v1;
-            var div = v / v1;
+            //int v = 10;
+            //int v1 = 34;
+            //var su = v+v1;
+            //var re = v - v1;
+            //var mul = v * v1;
+            //var div = v / v1;
             //cualquier tipo de dato al realizar la operacion 
 
-            Console.WriteLine("{0}", su);
-            Console.WriteLine("{0}", re);
-            Console.WriteLine("{0}", mul);
-            Console.WriteLine("{0}", div);
+            //Console.WriteLine("{0}", su);
+            //Console.WriteLine("{0}", re);
+            //Console.WriteLine("{0}", mul);
+            //Console.WriteLine("{0}", div);
+
+            //OPERADORES DE ASIGNACION
+
+            // int v = 10;
+            //int v1 = 34;
+            //v += v1; //incremento
+            //v += v1;// decremento
+            //v *= v1; //multiplicacion
+            //v /= v1;//divide y asigna
+            //v %= v1// divide el dato y muestra el residuo
+
+            //Console.WriteLine("{0}", v);
+
+            //OPERADORES DE COMPROBACION DE TIPOS Y RELACIONES
+            //int v = 10;
+            //int v1 = 34;
+            //var data = v < v1;//comparar
+            //var data1 = v > v1;
+            //var data2 = v >= v1;
+            //var data3 = v <= v1;
+            //var data4 = v == v1;
+            //var data5 = v != v1;
+            //Console.WriteLine("{0}", data);
+            //Console.WriteLine("{0}", data1);
+            //Console.WriteLine("{0}", data2);
+            //Console.WriteLine("{0}", data3);
+            //Console.WriteLine("{0}", data4);
+            //Console.WriteLine("{0}", data5);
+
+            //IF Y OP CONDICIONALES
+
+            int v = 23;
+            int v1 = 12;
+            double v2 = 10;
+            double v3 = 13;
+            var data5 = v != v1;
+
+            if (data5)
+            {
+                Console.WriteLine("{0}", data5);
+            }
+
+            if(v != v1 )
+            {
+                Console.WriteLine("{0}", data5);
+            }
+
+            if (v != v1  && v2 == v3)
+            {
+                Console.WriteLine("rt{0}", data5);
+            }
+            if (v != v1 || v2 == v3)
+            {
+                Console.WriteLine("rta{0}", data5);
+            }
+
+
+
+
+
+
+
+
+
+
 
 
             Console.ReadLine();
