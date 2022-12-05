57f9836cb1d6f727f3f6d162952ead1104ad4907
Yennifer Ortega Ocampo <Yenniferso.ortegaoc@ecci.Edu.co>
2022-12-04 21:05:34 -0500

    aritmetica

--- Program.cs
+++ Program.cs
@@ -6,12 +6,42 @@ namespace PROYECTO1NETCORE
     {
         static void Main(string[] args)
         {
-            int year = 10;
-            int twoyear = Convert.ToInt16("convertir datos de tipo String a int");
-            double decimales = 2.5;
-            string nombre = "joe";
+            //int year = 10;
+            //int twoyear = Convert.ToInt16("convertir datos de tipo String a int");//
+            //double decimales = 2.5;
+            //string nombre = "joe";
+            //String nombres = "Alex";
+            //decimal decimals = 2.8M;
+            //float flotante = 8.0F;
+            // decimal se puede usar cuando se tenga una cifra exacta, las float y double ocupan menos espacio//
 
-            Console.WriteLine("Holaaaaaa tengo {0} "+ year + " años "+ decimales + nombre);
+            //bool valor = false;
+            //falso o verdadero
+            //char letra = 'a';
+            //nsolo almacena un caracter en comillas simples
+            //Decimal de = 3.32M;
+            //Boolean valor1 = false;
+            //solo maneja true o false
+
+            //Console.WriteLine("Holaaaaaa tengo {0} {1} {2} {3} {4}", year , decimales, nombre, nombres, decimals);
+
+            //Console.WriteLine("{0} {1} {2} {3}", valor, letra,de, valor1);
+
+            //OPERADORES ARITMETICOS
+
+            int v = 10;
+            int v1 = 34;
+            var su = v+v1;
+            var re = v - v1;
+            var mul = v * v1;
+            var div = v / v1;
+            //cualquier tipo de dato al realizar la operacion 
+
+            Console.WriteLine("{0}", su);
+            Console.WriteLine("{0}", re);
+            Console.WriteLine("{0}", mul);
+            Console.WriteLine("{0}", div);
+
 
             Console.ReadLine();
 
