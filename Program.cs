8c1bf6397787ce62589be473822ab5677197a5a8
yennifer.ortega <yenniferso.ortegaoc@ecci.edu.co>
2022-12-12 14:15:08 -0500

    array bi y tridimensional

--- Program.cs
+++ Program.cs
@@ -6,24 +6,13 @@ namespace pruebaslogin
     {
         static void Main(string[] args)
         {
-            int v = 9;
-            int v1 = 9;
-            double v2 = 11;
-            double v3 = 11;
-            string name;
-            var data = v != v1;
+            // Array bidimensional
+            double[,] doble = new double[2, 2] { { 6, 3.2 }, { 6, 5.6 } };
+            Console.WriteLine("rta {0}", doble[0,1]);
 
-            if (data)
-            {
-                name = "Alex";
-            }
-            else
-            {
-                name = "Joel";
-            }
-            name = data ? "Alex" : "Joel";
-            Console.WriteLine("rta {0}", name);
-
+            // Array tridimensional
+            double[,,] triple = new double[2, 2, 3] { { { 3.0, 6.9, 8.9 }, { 6.8, 5.6, 7.6 } }, { { 3.8, 4.3, 3.2 }, { 3.8, 10.6, 4.5 } } };
+            Console.WriteLine("rta {0}", triple[0, 1, 2]);
             Console.ReadLine();
         
 
