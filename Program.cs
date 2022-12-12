eb0f1bc687ec8f2c779c5fa417ab3c61f84653c4
yennifer.ortega <yenniferso.ortegaoc@ecci.edu.co>
2022-12-12 13:43:49 -0500

    array int

--- Program.cs
+++ Program.cs
@@ -13,10 +13,17 @@ namespace pruebaslogin
             cadenas[3] = "Luis ";
             cadenas[4] = "Carlos ";
             string[] name = {"Joel", " PDHN", "Pablito ", };
+            int[] age = new int[5];
+            age[0] = 5;
+            age[1] = 15;
+            age[2] = 50;
+            age[3] = 55;
+            age[4] = 95;
+
 
             Console.WriteLine("rta {0} {1}  {2}  {3} {4}", cadenas[0], cadenas[1], cadenas[2], cadenas[3], cadenas[4]);
             Console.WriteLine("rta {0}", cadenas[0] + " " + cadenas[3]);
-            Console.WriteLine("rta {0}", cadenas[3] +" "+ name[2s]);
+            Console.WriteLine("rta {0}", cadenas[3] +" "+ name[2] + " age " + age[2]);
             Console.ReadLine();
 
 
