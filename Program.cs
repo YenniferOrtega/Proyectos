0574b5d4385ef48a6a3ad350e85f980a89690fe8
yennifer.ortega <yenniferso.ortegaoc@ecci.edu.co>
2022-12-12 11:46:42 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace pruebaslogin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v = 9;
+            int v1 = 9;
+            double v2 = 11;
+            double v3 = 11;
+            string name;
+            var data = v != v1;
+
+            if (data)
+            {
+                name = "Alex";
+            }
+            else
+            {
+                name = "Joel";
+            }
+            name = data ? "Alex" : "Joel";
+            Console.WriteLine("rta {0}", name);
+
+            Console.ReadLine();
+        
+
+
+
+        }
+    }
+}
