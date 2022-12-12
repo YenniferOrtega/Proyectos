fa9d2f2563ee2cd65b4b28fc623ef4096ba550ae
yennifer.ortega <yenniferso.ortegaoc@ecci.edu.co>
2022-12-12 13:34:47 -0500

    array de tipo string

--- Program.cs
+++ Program.cs
@@ -6,29 +6,20 @@ namespace pruebaslogin
     {
         static void Main(string[] args)
         {
-            int v = 9;
-            int v1 = 4;
-            double v2 = 11;
-            double v3 = 11;
-            string name;
-            var data = v != v1;
+            string[] cadenas = new string[5];
+            cadenas[0] = "Yennifer ";
+            cadenas[1] = "jose ";
+            cadenas[2] = "Pablito ";
+            cadenas[3] = "Luis ";
+            cadenas[4] = "Carlos ";
+            string[] name = {"Joel", " PDHN", "Pablito ", };
 
-            if (data)
-            {
-                name = "Alex";
-            }
-            else
-            {
-                name = "Joel";
-            }
-
-            name = data ? "Alex" : "Joel";
-
-
-            Console.WriteLine("rta {0}", name);
-
+            Console.WriteLine("rta {0} {1}  {2}  {3} {4}", cadenas[0], cadenas[1], cadenas[2], cadenas[3], cadenas[4]);
+            Console.WriteLine("rta {0}", cadenas[0] + " " + cadenas[3]);
+            Console.WriteLine("rta {0}", cadenas[3] +" "+ name[2s]);
             Console.ReadLine();
-        
+
+
 
 
 
